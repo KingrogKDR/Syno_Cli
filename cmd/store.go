9ae58ec5fc8cb30a410a327092eaaa0d1714a7a4
KingrogKDR <abhisheksaikia766@gmail.com>
2025-08-11 12:57:08 +0530

    feat:replacing and creating existing files added

--- cmd/store.go
+++ cmd/store.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
-	"google.golang.org/api/drive/v3"
 	"log"
 	"os"
 	"path/filepath"
 	"syno/misc"
 	storageconfig "syno/storageConfig"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/api/drive/v3"
 )
 
 var storeCmd = &cobra.Command{
@@ -23,21 +24,110 @@ var storeCmd = &cobra.Command{
 			return err
 		}
 		file_Path := args[0]
-		file, err := os.Open(file_Path)
 		baseFilename := filepath.Base(file_Path)
+
+		query := fmt.Sprintf("name = '%s' and trashed = false", baseFilename)
+		r, err := server.Files.List().Q(query).Fields("files(id, name)").Do()
 		if err != nil {
-			misc.DefineError(err, "Unable to open file")
+			misc.DefineError(err, "Unable to search for existing files")
 			return err
 		}
-		defer file.Close()
 
-		driveFile, err := server.Files.Create(&drive.File{Name: baseFilename}).Media(file).Do()
+		if len(r.Files) > 0 {
+			existingFile := r.Files[0]
+			fmt.Printf("A file named '%s' already exists (ID: %s).\n", existingFile.Name, existingFile.Id)
+			fmt.Print("Do you want to [r]eplace it or [c]reate a new file? (r/c): ")
 
-		if err != nil {
-			misc.DefineError(err, "Unable to upload file to google drive")
-			return err
+			var choice string
+			_, err := fmt.Scanln(&choice)
+			if err != nil || (choice != "r" && choice != "R" && choice != "c" && choice != "C") {
+				fmt.Println("Invalid choice. Aborting upload.")
+				return nil
+			}
+
+			if choice == "r" || choice == "R" {
+				return replaceFile(file_Path, server, baseFilename, existingFile.Id)
+			} else {
+				return createUniqueFile(server, baseFilename, file_Path)
+			}
 		}
-		log.Printf("File successfully uploaded! ID: %s, Name: %s\n", driveFile.Id, driveFile.Name)
-		return nil
+		return createNewFile(file_Path, server, baseFilename)
 	},
+}
+
+func createNewFile(file_Path string, server *drive.Service, baseFilename string) error {
+	file, err := os.Open(file_Path)
+	if err != nil {
+		misc.DefineError(err, "Unable to open file in createNewFile")
+		return err
+	}
+	defer file.Close()
+
+	driveFile, err := server.Files.Create(&drive.File{Name: baseFilename}).Media(file).Do()
+
+	if err != nil {
+		misc.DefineError(err, "Unable to upload file to Google Drive")
+		return err
+	}
+
+	log.Printf("New file successfully uploaded! ID: %s, Name: %s\n", driveFile.Id, driveFile.Name)
+
+	return nil
+}
+
+func replaceFile(file_Path string, server *drive.Service, baseFilename string, fileID string) error {
+	file, err := os.Open(file_Path)
+	if err != nil {
+		misc.DefineError(err, "Unable to open file in createNewFile")
+		return err
+	}
+	defer file.Close()
+
+	driveFile, err := server.Files.Update(fileID, &drive.File{Name: baseFilename}).Media(file).Do()
+	if err != nil {
+		misc.DefineError(err, "Unable to replace file in Google Drive")
+		return err
+	}
+
+	log.Printf("File '%s' successfully replaced! ID: %s\n", driveFile.Name, driveFile.Id)
+	return nil
+}
+
+func createUniqueFile(server *drive.Service, baseFilename string, file_Path string) error {
+	ext := filepath.Ext(baseFilename)
+	nameWithoutExt := baseFilename[:len(baseFilename)-len(ext)]
+	i := 1
+	var uniqueFilename string
+
+	for {
+		uniqueFilename = fmt.Sprintf("%s (%d)%s", nameWithoutExt, i, ext)
+		query := fmt.Sprintf("name = '%s' and trashed = false", uniqueFilename)
+		r, err := server.Files.List().Q(query).Fields("files(id)").Do()
+		if err != nil {
+			misc.DefineError(err, "Unable to check for unique filename")
+			return err
+		}
+
+		if len(r.Files) == 0 {
+			// Found a unique name, break the loop
+			break
+		}
+
+		i++
+	}
+	file, err := os.Open(file_Path)
+	if err != nil {
+		misc.DefineError(err, "Unable to open file in createUniqueFile")
+		return err
+	}
+	defer file.Close()
+
+	driveFile, err := server.Files.Create(&drive.File{Name: uniqueFilename}).Media(file).Do()
+	if err != nil {
+		misc.DefineError(err, "Unable to upload file to Google Drive with a unique name")
+		return err
+	}
+
+	log.Printf("New file successfully uploaded! ID: %s, Name: %s\n", driveFile.Id, driveFile.Name)
+	return nil
 }
--- misc/constants.go
+++ misc/constants.go
@@ -1,3 +1,16 @@
 package misc
 
-const PathToCredentials = "credentials.json"
+import (
+	"os"
+	"path/filepath"
+)
+
+var PathToCredentials string
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		DefineError(err, "Unable to get user home directory")
+	}
+	PathToCredentials = filepath.Join(homeDir, "Documents", "Syno", "credentials.json")
+}
